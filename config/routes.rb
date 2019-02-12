cdc7d2028b88596bce1fb9ab489875d58770b6e6
高山　善成 <takayamayoshinari@kouzanyoshinarinoMacBook-Air.local>
2019-02-12 11:20:48 +0900

    signup view and user view

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get '/signup',to:'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users
 end
